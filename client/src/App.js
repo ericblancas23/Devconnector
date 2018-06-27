b570bef5ce905d5cf06a20b6fa424290e0dff141
EricB <ericblancas23@gmail.com>
2018-06-26 20:09:53 -0700

    file changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+import Landing from './components/Landing';
+import { Footer } from './components/Footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>test</h1>
-      </div>
+      <Router>
+         <div className='App'>
+         <Navbar />
+             <Route exact path='/' component={ Landing } />
+           <div className="container">
+             <Route path='/Login' component={ Login } />
+             <Route path='/Register' component={ Register } />
+           </div>
+          <Footer />
+         </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Footer = () => {
+   return(
+       <footer className="bg-dark text-white mt-5 p-4 text-center">
+        Copyright & {new Date().getFullYear()} DevConnector
+       </footer>
+
+   )
+}
